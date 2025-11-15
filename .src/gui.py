407895893a84ca357415eaad2bbaf651d3895286
Nickshg01 <nicolasdeoliveira548@gmail.com>
2025-11-15 11:38:52 -0300

    Criação da interface básica

--- .src/gui.py
+++ .src/gui.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import (QMainWindow, QWidget, QGridLayout, QPushButton)
 from PySide6.QtGui import QIcon
-from values import ICON
+from values import (ICON, BUTTON_SIZE, BIG_FONT_SIZE)
 
 # CRIA A INTERFACE PRINCIPAL
 
@@ -13,3 +13,24 @@ class MainWindow(QMainWindow):
         self.centralWidget().setLayout(self._layout)
         self.setWindowTitle('Jogo da Velha')
         self.setWindowIcon(QIcon(str(ICON)))
+
+        self._addButtons()
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
+
+# PADRONIZA OS BOTÕES
+    class Button(QPushButton):
+        def __init__(self, text: str = ''):
+            super().__init__(text)
+            
+            self.setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+            if text == 'X':
+                self.setStyleSheet(f'color: red; font-size: {BIG_FONT_SIZE}px')
+            elif text == 'O':
+                self.setStyleSheet(f'color: blue; font-size: {BIG_FONT_SIZE}px')
+
+# ADICIONA OS BOTÕES AO LAYOUT
+    def _addButtons(self):
+        for i in range(3):
+            for j in range(3):
+                self._layout.addWidget(self.Button(), i, j)
--- .src/values.py
+++ .src/values.py
@@ -3,3 +3,8 @@ from pathlib import Path
 # DECLARA O CAMINHO DO ÍCONE DA JANELA
 
 ICON = Path(__file__).parent.parent / 'files' / 'icon.png'
+
+# CONSTANTES DE TAMANHO
+
+BUTTON_SIZE = 130
+BIG_FONT_SIZE = 80
