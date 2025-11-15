62451d67dbf4a545cd6522813db3ef0a007da0c2
Nickshg01 <nicolasdeoliveira548@gmail.com>
2025-11-15 13:38:49 -0300

    Implementação do algoritmo Minimax

--- .src/tictactoe.py
+++ .src/tictactoe.py
@@ -1,7 +1,7 @@
 import math
 from values import (HUMAN_PLAYER, AI_PLAYER)
 
-# LÓGICA DE PROGRAMAÇÃO
+# LÓGICA BACKEND
 
 class TicTacToe:
     def __init__(self):
@@ -11,7 +11,11 @@ class TicTacToe:
         '''Retorna quais espaços estão livres.'''
         return [i for i, spot in enumerate(self.board) if spot == '']
     
-    def _make_move(self, position, player) -> bool:
+    def _board_is_full(self) -> bool:
+        '''Verifica se o tabuleiro está cheio.'''
+        return self._avaliable_moves == []
+    
+    def _make_move(self, position: int, player: str) -> bool:
         '''Faz um movimento caso o espaço estiver vazio e retorna True. Caso contrário, retorna False.'''
         if self.board[position] == '':
             self.board[position] = player
@@ -35,4 +39,37 @@ class TicTacToe:
     
     def _game_over(self) -> bool:
         '''Verifica se o jogo encerrou.'''
-        return self._winner_check() is not None or self._avaliable_moves() == []
+        return self._winner_check() is not None or self._board_is_full()
+    
+    def _minimax(self, depth: int, is_maximizing: bool):
+        '''Algoritmo Minimax. Retorna a melhor pontuação possível para o tabuleiro atual.'''
+        winner = self._winner_check()
+
+        if winner == AI_PLAYER:
+            return 1
+        elif winner == HUMAN_PLAYER:
+            return -1
+        elif self._board_is_full():
+            return 0
+        
+        if is_maximizing:
+            best_score = float('-inf')
+            
+            for move in self._avaliable_moves():
+                self._make_move(move, AI_PLAYER)
+                score = self._minimax(depth+1, False)
+                self.board[move] = ''
+                best_score = max(score, best_score)
+            return best_score
+        
+        else:
+            best_score = float('inf')
+
+            for move in self._avaliable_moves():
+                self._make_move(move, HUMAN_PLAYER)
+                score = self._minimax(depth+1, True)
+                self.board[move] == ''
+                best_score = min(score, best_score)
+            return best_score
+
+TicTacToe()
